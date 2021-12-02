b36d2a40a0048c223c3922479bdb449d03287921
DominicLumsden <Lumsdend@bzz.ch>
2021-12-02 15:53:08 +0100

    final removed

--- src/main/java/ch/roomManager/models/Event.java
+++ src/main/java/ch/roomManager/models/Event.java
@@ -6,15 +6,9 @@ import lombok.Data;
 @Data
 @Builder(toBuilder = true)
 public class Event {
-    private final int id;
+    private int id;
     private String title;
     private String description;
     private String organiser;
 
-    public Event(int id, String title, String description, String organiser) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.organiser = organiser;
-    }
 }
--- src/main/java/ch/roomManager/models/Room.java
+++ src/main/java/ch/roomManager/models/Room.java
@@ -3,17 +3,13 @@ package ch.roomManager.models;
 import lombok.Builder;
 import lombok.Data;
 
+
     @Data
     @Builder(toBuilder = true)
     public class Room {
-        private final int id;
+        private int id;
         private String name;
         private String description;
 
-        public Room(int id, String name, String description) {
-            this.id = id;
-            this.name = name;
-            this.description = description;
-        }
     }
 
